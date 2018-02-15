1d7a76688beda2be3267ab4b695d95b1b7b5fb5c
ALMSIVI <zakarumbeliever@hotmail.com>
2018-02-14 21:32:31 -0800

    Flashback mode layout

--- app/src/main/java/cse110/team19/flashbackmusic/PlayList_Activity.java
+++ app/src/main/java/cse110/team19/flashbackmusic/PlayList_Activity.java
@@ -8,33 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class PlayList_Activity extends AppCompatActivity {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_list_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
-
-
 }
