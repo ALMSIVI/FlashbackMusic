178fdbf82091141d67aaa5dcd3d885f033060868
KunalParulekar <iamkunal.parulekar@gmail.com>
2018-02-15 22:39:12 -0800

    Fixed activity switching

--- app/src/main/java/cse110/team19/flashbackmusic/PlayList_Activity.java
+++ app/src/main/java/cse110/team19/flashbackmusic/PlayList_Activity.java
@@ -6,13 +6,27 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class PlayList_Activity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_list_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button normalButton = (Button)findViewById(R.id.normalModeButton);
+
+        normalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
