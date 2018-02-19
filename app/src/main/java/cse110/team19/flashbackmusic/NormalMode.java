930d1a06a8ee3bcdc2d8b8a48d0cad5113be8d7f
ALMSIVI <zakarumbeliever@hotmail.com>
2018-02-18 18:32:33 -0800

    Fixed sharedPreferences

--- app/src/main/java/cse110/team19/flashbackmusic/NormalMode.java
+++ app/src/main/java/cse110/team19/flashbackmusic/NormalMode.java
@@ -44,7 +44,7 @@ public class NormalMode extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String mode = sharedPreferences.getString("mode", "");
         if (mode.equals("Flashback")) {
             switchFlashback(null);
@@ -180,37 +180,6 @@ public class NormalMode extends AppCompatActivity {
                 album_data.get(albumName).addTrack(t);
                 album_to_tracks.get(album_data.get(albumName)).add(t);
             }
-
-            // Retrieve data from sharedPreferences
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-            int status = sharedPreferences.getInt(t.getTrackName() + "Status", 0);
-            t.setStatus(status);
-
-
-            // calendar
-            String cal = sharedPreferences.getString(t.getTrackName() + "Time", null);
-            SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
-            if (cal != null) {
-                try {
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.setTime(format.parse(cal));
-                    t.setCalendar(calendar);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            // location
-            String loc = sharedPreferences.getString(t.getTrackName() + "Location", "Unknown Location");
-            if (!loc.equals("Unknown Location")) {
-                String[] locationValue = loc.split("");
-                double latitude = Double.parseDouble(locationValue[0]);
-                double longitude = Double.parseDouble(locationValue[1]);
-                Location location = new Location("");
-                location.setLatitude(latitude);
-                location.setLongitude(longitude);
-                t.setLocation(location);
-            }
         }
     }
 
@@ -224,7 +193,7 @@ public class NormalMode extends AppCompatActivity {
             mediaPlayer.stop();
         }
         // Change the mode in sharedpreferences
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("mode", "Flashback");
         editor.apply();
