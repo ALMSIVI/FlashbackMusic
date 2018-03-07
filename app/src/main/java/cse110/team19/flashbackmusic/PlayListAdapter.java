4c240970952969d8022a2a0c5e390b56557e6be0
Sarah Ji <s9ji@ucsd.edu>
2018-03-06 18:30:27 -0800

    can i play a song yet

--- app/src/main/java/cse110/team19/flashbackmusic/PlayListAdapter.java
+++ app/src/main/java/cse110/team19/flashbackmusic/PlayListAdapter.java
@@ -128,9 +128,21 @@ public class PlayListAdapter extends BaseAdapter {
             view = layoutInflater.inflate(R.layout.playlist_view, null);
         }
 
-        TextView track_name = (TextView) view.findViewById(R.id.track_name);
-        track_name.setText(track.getTrackName());
+        TextView new_track = (TextView) view.findViewById(R.id.track_name);
+        new_track.setText(track.getTrackName());
 
+        new_track.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (track.getStatus() > -1) {
+                    changePlayPause();
+                    int id = track.getResourceId();
+                    audioResourceId = new <Pair<Integer, Track>>ArrayList();
+                    audioResourceId.add(new Pair<Integer, Track>(id, track));
+                    loadMedia(id, track);
+                }
+            }
+        });
 
         final Button status_button = (Button) view.findViewById(R.id.set_status);
         changeButton(track, status_button);
