aaa2c72fdf45c603223533b939df8616127eab93
Meeta Marathe <mbmarath@ucsd.edu>
2018-03-13 20:18:33 -0700

    Getting google sign in result to not return null

--- app/src/main/java/cse110/team19/flashbackmusic/MainActivity.java
+++ app/src/main/java/cse110/team19/flashbackmusic/MainActivity.java
@@ -135,9 +135,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         // Google Signin Activity
         SignIn = (SignInButton) findViewById(R.id.main_googlesigninbtn);
         SignIn.setOnClickListener(this);
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
         googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
+                //.enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions)
                 .build();
 
@@ -426,7 +428,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         }
 
         else {
-            result.getStatus().getStatusMessage();
+            //Log.d("result error message", result.getStatus().getStatusMessage());
         }
     }
 
@@ -471,6 +473,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
 
         alert.show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (googleApiClient != null && googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+        super.onStop();
     }
 
     //endregion
